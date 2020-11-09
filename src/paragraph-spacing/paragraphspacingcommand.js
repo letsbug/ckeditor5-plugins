af3867bf666bbdea89cd714fc7a5e441fe7b2a7c
letsbug <biuabiu@qq.com>
2020-11-09 18:25:08 +0800

    news: ·新增段落间距插件(coding...)

--- src/paragraph-spacing/paragraphspacingcommand.js
+++ src/paragraph-spacing/paragraphspacingcommand.js
@@ -2,13 +2,20 @@
  * @module indent-first/paragraphspacingcommand
  */
 import Command from '@ckeditor/ckeditor5-core/src/command';
+import first from '@ckeditor/ckeditor5-utils/src/first';
+
+const ATTRIBUTE = 'paragraphSpacing';
 
 export default class ParagraphSpacingCommand extends Command {
 	/**
 	 * @inheritDoc
 	 */
 	refresh() {
-		//
+		const firstBlock = first(this.editor.model.document.selection.getSelectedBlocks());
+
+		this.isEnabled = !!firstBlock && this._canSetAttribute(firstBlock);
+
+		this.value = this.isEnabled && firstBlock.hasAttribute(ATTRIBUTE) ? firstBlock.getAttribute(ATTRIBUTE) : '1';
 	}
 
 	/**
@@ -16,5 +23,9 @@ export default class ParagraphSpacingCommand extends Command {
 	 */
 	execute() {
 		//
+	}
+
+	_canSetAttribute(block) {
+		return this.editor.model.schema.checkAttribute(block, ATTRIBUTE);
 	}
 }
