33d9693d56c0b874fa5f6e7a7cb624868adef705
letsbug <biuabiu@qq.com>
2020-11-15 16:38:32 +0800

    optimizes: ·清除空白字符插件添加额外的正则逻辑，只清除换行符之外的其他空白字符.

--- src/clear-space/utils.js
+++ src/clear-space/utils.js
@@ -1,3 +1,6 @@
+// All white space characters except '\n'
+const character = ' \\f\\r\\t\\v\\u00a0\\u1680\\u180e\\u2000-\\u200a\\u2028\\u2029\\u202f\\u205f\\u3000\\ufeff';
+
 /**
  * judgment whether the clearSpace button can be executed
  *
@@ -6,15 +9,15 @@
  */
 export function clearSpaceExecutable(blocks) {
 	return blocks.some((b) => {
-		if (b.isEmpty) {
-			return true;
+		if (b.isEmpty || (b.data && b.data.trim() === '')) {
+			return false;
 		}
 		const text = Array.from(b.getChildren())
 			.map((c) => c.data)
 			.join('');
 		// begin with blank || end with blanck || contains more than 2 white space characters
 		// 以空白开头 || 以空白结尾 || 包含2个以上空白字符
-		return /^\s+[\S\w]+/.test(text) || /[\S\w]+\s+$/.test(text) || /[\S\w]+?[\s]{2}[\S\w]+?/g.test(text);
+		return new RegExp(`^[${character}]+`).test(text) || new RegExp(`[${character}]+$`).test(text) || new RegExp(`[${character}]{2}`, 'g').test(text);
 	});
 }
 
@@ -31,33 +34,29 @@ export function clearSpace(writer, blocks) {
 			return;
 		}
 
-		const props = {};
-		Array.from(b.getAttributeKeys()).forEach((key) => {
-			props[key] = b.getAttribute(key);
-		});
-
-		const replace = writer.createElement(b.name, props);
+		const replace = writer.createElement(b.name, Object.fromEntries(b.getAttributes()));
 		const childes = Array.from(b.getChildren());
 
-		childes.forEach((textNode, i) => {
-			if (!textNode.is('$text')) {
-				return;
-			}
+		childes.forEach((node, i) => {
+			const attrs = Object.fromEntries(node.getAttributes());
+			if (node.is('$text')) {
+				// let text = node.data.replace(/\s+/g, ' ');
+				let text = node.data.replace(new RegExp(`[${character}]+`, 'g'), ' ');
+				// if (i === 0 || (i > 0 && /\s+$/.test(childes[i - 1].data))) {
+				if (i === 0 || (i > 0 && new RegExp(`[${character}]+$`).test(childes[i - 1].data))) {
+					// text = text.replace(/^\s+/, '');
+					text = text.replace(new RegExp(`^[${character}]+`), '');
+				}
+				if (i > 0 && i === childes.length - 1) {
+					// text = text.replace(/\s+$/, '');
+					text = text.replace(new RegExp(`[${character}]+$`), '');
+				}
 
-			const nats = {};
-			Array.from(textNode.getAttributeKeys()).forEach((key) => {
-				nats[key] = textNode.getAttribute(key);
-			});
-
-			let text = textNode.data.replace(/\s+/g, ' ');
-			if (i === 0 || (i > 0 && /\s+$/.test(childes[i - 1].data))) {
-				text = text.replace(/^\s+/, '');
+				writer.appendText(text, attrs, replace);
+			} else {
+				// May contain non-text element nodes (eg: softBreak)
+				writer.appendElement(node.name, attrs, replace);
 			}
-			if (i > 0 && i === childes.length - 1) {
-				text = text.replace(/\s+$/, '');
-			}
-
-			writer.appendText(text, nats, replace);
 		});
 
 		writer.insert(replace, b, 'after');
