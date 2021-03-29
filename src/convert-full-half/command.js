17bb939b666f4c8c6d5df5b4562434512632f4b6
letsbug <biuabiu@qq.com>
2021-03-29 16:32:33 +0800

    optimizes: ·全角半角转换插件替换逻辑优化，现在能应用于行级，以及鼠标选择区域

--- src/convert-full-half/command.js
+++ src/convert-full-half/command.js
@@ -3,7 +3,6 @@
  */
 
 import Command from '@ckeditor/ckeditor5-core/src/command';
-import { findFirst } from '../utils';
 
 const dictionary = {
 	full: '《》（）【】。？！；，：“”‘’',
@@ -16,164 +15,91 @@ export default class ConvertFullHalfCommand extends Command {
 	 * @inheritDoc
 	 */
 	refresh() {
-		const iterator = this.editor.model.document.selection.getSelectedBlocks();
-
-		if (!iterator) {
+		const model = this.editor.model;
+		const selection = model.document.selection;
+		const start = selection.getFirstPosition();
+		const end = selection.getLastPosition();
+		if (!start || !end) {
 			this.isEnabled = false;
+			this.range = null;
 			return;
 		}
 
-		this.isEnabled = this._executable(this.editor.model.document.selection.getSelectedBlocks());
-		if (!this.isEnabled) {
-			return;
-		}
-
-		this.value = this._findExecuteType(iterator);
+		this.range = model.createRange(start, end);
+		this.isEnabled = this._executable();
 	}
 
 	/**
 	 * Executes the command. convert half to full or full to half.
 	 *
 	 * @param options
-	 * @fires execute
 	 */
 	execute(options = {}) {
+		if (!this.range) {
+			return;
+		}
+
 		const model = this.editor.model;
-		const iterator = model.document.selection.getSelectedBlocks();
 		const type = options.type || this.value;
 
 		model.change((writer) => {
-			for (const block of iterator) {
-				if (this._exclude(block.name)) {
+			for (const walker of this.range.getWalker()) {
+				const textNode = walker.item.textNode;
+				if (!textNode) {
 					continue;
 				}
 
-				const replace = [];
-				for (const node of block.getChildren()) {
-					if (node.is('$text')) {
-						replace.push(writer.createText(this._cycle(node, type), node.getAttributes()));
-					} else {
-						replace.push(node);
-					}
-					writer.remove(node);
+				const text = walker.item.data;
+				const matcher = new RegExp(`[${dictionary[{ full: 'full', half: 'halfEscaped' }[type]]}]`, 'g');
+				const matched = text.match(matcher);
+				if (!matched || !matched.length) {
+					continue;
 				}
 
-				replace.forEach((n) => writer.append(n, block));
+				const start = textNode.data.indexOf(text);
+				const end = textNode.data.lastIndexOf(text);
+				const range = writer.createRange(
+					writer.createPositionAt(textNode.parent, ~start ? start : textNode.startOffset),
+					writer.createPositionAt(textNode.parent, ~end ? end + text.length : textNode.endOffset)
+				);
+				let replace = text;
+
+				for (const char of matched) {
+					const position = dictionary[type].indexOf(char);
+					let _regStr = `${char}`;
+					if (['<', '>', '(', ')', '[', ']', '.', '?', '!'].includes(char)) {
+						_regStr = '\\' + _regStr;
+					}
+					replace = replace.replace(
+						new RegExp(_regStr, 'g'),
+						dictionary[{ full: 'half', half: 'full' }[type]].charAt(position)
+					);
+				}
+				model.insertContent(writer.createText(replace), range);
 			}
 		});
-	}
-
-	/**
-	 * convert characters
-	 * TODO need optimizes logics
-	 *
-	 * @param node {module:engine/model/node~Node}
-	 * @param type {'full'|'half'}
-	 */
-	_cycle(node, type) {
-		const old = node.data;
-		const matcher = new RegExp(`[${type === 'full' ? dictionary.full : dictionary.halfEscaped}]`, 'g');
-		const matched = old.matchAll(matcher);
-		if (!matched) {
-			return old;
-		}
-
-		const matchedArr = Array.from(matched);
-		if (!matchedArr.length) {
-			return old;
-		}
-
-		let str = old;
-		const standby = {
-			full: 'half',
-			half: 'full',
-		};
-		matchedArr.forEach(([char]) => {
-			const position = dictionary[type].indexOf(char);
-			let _regStr = `${char}`;
-			if (['<', '>', '(', ')', '[', ']', '.', '?', '!'].includes(char)) {
-				_regStr = '\\' + _regStr;
-			}
-			str = str.replace(new RegExp(_regStr, 'g'), dictionary[standby[type]].charAt(position));
-		});
-
-		return str;
-	}
-
-	/**
-	 * Find the convertFullHalf command execute type,
-	 * If the first character found is a half Angle, the command performs a half Angle conversion,
-	 * Otherwise, a full Angle conversion is performed.
-	 *
-	 * @param blocks
-	 * @returns {String|null}
-	 */
-	_findExecuteType(blocks) {
-		let type = null;
-
-		const findFirstNode = (node) => {
-			if (!node.is('$text')) {
-				return false;
-			}
-
-			const data = node.data;
-			let matched = data.match(new RegExp(`[${dictionary.full}${dictionary.halfEscaped}]`));
-			if (matched && matched.length) {
-				type = dictionary.full.includes(matched[0]) ? 'full' : 'half';
-				return true;
-			}
-
-			return false;
-		};
-
-		findFirst(blocks, (block) => !block.isEmpty && findFirst(block.getChildren(), findFirstNode));
-
-		return type;
-	}
-
-	_exclude(name) {
-		return ['image', 'media', 'table'].includes(name);
-	}
-
-	/**
-	 * Identify whether a string contains full-angle characters
-	 *
-	 * @param str
-	 * @returns {boolean}
-	 */
-	_hasFull(str) {
-		return new RegExp(`[${dictionary.full}]`, 'g').test(str);
-	}
-
-	/**
-	 * Identify whether a string contains half-angle characters
-	 *
-	 * @param str
-	 * @returns {boolean}
-	 */
-	_hasHalf(str) {
-		return new RegExp(`[${dictionary.halfEscaped}]`, 'g').test(str);
-	}
-
-	/**
-	 * Identify whether the node can be convert characters
-	 *
-	 * @param nodes
-	 * @returns {boolean}
-	 */
-	_checkNodes(nodes) {
-		const first = findFirst(nodes, (item) => this._hasFull(item.data) || this._hasHalf(item.data));
-		return !!first;
 	}
 
 	/**
 	 * Identify whether the selections can be convert characters
 	 *
-	 * @param iterator
 	 * @returns {boolean}
 	 */
-	_executable(iterator) {
-		const callback = (item) => !item.isEmpty && !this._exclude(item.name) && this._checkNodes(item.getChildren());
-		return !!findFirst(iterator, callback);
+	_executable() {
+		for (const walker of this.range.getWalker()) {
+			const textNode = walker.item.textNode;
+			if (!textNode) {
+				continue;
+			}
+
+			const text = walker.item.data;
+			const matched = text.match(new RegExp(`[${dictionary.full}${dictionary.halfEscaped}]`));
+			if (matched && matched.length) {
+				this.value = dictionary.full.includes(matched[0]) ? 'full' : 'half';
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
